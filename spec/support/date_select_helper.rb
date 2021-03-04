6b050f2c749515e84b391005a133a3a29dc89fca
mogarinomiya <cindy_ira@hotmail.com>
2021-03-05 00:16:03 +0900

    日付選択のヘルパーメソッド

---
+++ spec/support/date_select_helper.rb
@@ -0,0 +1,15 @@
+module DateSelectHelpers
+  def date_select(date, options = {})
+    year, month, day = date.split('-')
+
+    # 0始まりだとエラー返るので、数値型に直して0とってから文字列型に戻す
+    month = month.to_i
+    month = month.to_s
+    day = day.to_i
+    day = day.to_s
+
+    select year, from: 'user_birthday_1i'
+    select month, from: 'user_birthday_2i'
+    select day, from: 'user_birthday_3i'
+  end
+end
