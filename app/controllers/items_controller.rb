f529e2cf3df0b8006a07928b1b89fc9506cc1a43
mogarinomiya <cindy_ira@hotmail.com>
2021-03-06 16:38:42 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
     @items = Items.all
   end
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
