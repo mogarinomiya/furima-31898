28fdd2636dc408d7a13d6a31f4635677775703c6
mogarinomiya <cindy_ira@hotmail.com>
2021-03-07 21:51:55 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+  #active_hash :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_time_id
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :province
+  belongs_to :shipping_time
+
+  with_options presence: true do
+    validates :item_name
+    validates :image
+    validates :description
+    validates :price
+    validates :user
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_cost_id
+      validates :province_id
+      validates :shipping_time_id
+    end
+  end
+
 end
