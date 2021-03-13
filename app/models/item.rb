5ab36944ab85ca0fdf3335c3c3290a844db8c825
mogarinomiya <cindy_ira@hotmail.com>
2021-03-13 15:59:10 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,11 +17,11 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :user
     with_options numericality: { other_than: 1 } do
-      validates :category_id
-      validates :condition_id
-      validates :shipping_cost_id
-      validates :province_id
-      validates :shipping_time_id
+      validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 11 }
+      validates :condition_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 7 }
+      validates :shipping_cost_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 3 }
+      validates :province_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 48 }
+      validates :shipping_time_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 4 }
     end
   end
 
