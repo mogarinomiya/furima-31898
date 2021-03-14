300be5b0abd066ac238e26e6d3a23145df3c6f4a
mogarinomiya <cindy_ira@hotmail.com>
2021-03-15 02:03:30 +0900

    コントローラーにupdate,edit追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      redirect_to item: :show
+    else
+      render :edit
+    end
+  end
+
+  private
+
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_id, :province_id, :shipping_time_id, :price, :image).merge(user_id: current_user.id)
   end
