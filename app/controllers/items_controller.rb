b172b818781d7ab3a2d1f7e1c5434e8d4b697fd5
mogarinomiya <cindy_ira@hotmail.com>
2021-03-15 23:00:34 +0900

    共通部分のまとめ、投稿ユーザーのみ編集可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,18 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
-      redirect_to item: :show
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
@@ -41,4 +41,16 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_id, :province_id, :shipping_time_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def correct_user
+    user = Item.find(params[:id]).user_id
+    if current_user.id != user
+      redirect_to root_path
+    end
+  end
+
 end
