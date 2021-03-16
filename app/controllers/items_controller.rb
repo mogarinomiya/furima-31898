bde6a648c06ad72e785fa86658bb5566c2aafdac
mogarinomiya <cindy_ira@hotmail.com>
2021-03-16 11:29:30 +0900

    不要項目の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path(@item.id)
